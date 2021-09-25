a8ba623ef971f3f00606cb9022665d73d3b8f795
Jaareet <80985233+Jaareet@users.noreply.github.com>
2021-09-25 18:28:01 +0200

    The reals authors

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 
 game 'gta5'
 
-description 'Draw FPS Script FiveM by Jaareet#0097 & Carlooss#2021'
+description 'Draw FPS Script FiveM by Jaareet#0097 & Medinaa#7364'
 
 client_script 'client.lua'
 
